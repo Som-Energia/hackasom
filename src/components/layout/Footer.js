aa69c2b5d66e78e8b8931b1cf90994da77793528
David Riera <david.riera@somenergia.coop>
2021-05-27 14:35:10 +0200

    fixed footer with love

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -33,8 +33,8 @@ export default function Footer() {
           ))}
         </div>
         <p className="mt-8 text-center text-base text-gray-500">
-          &copy; Som Energia {new Date().getFullYear()} | Made with{" "}
-          <span className="text-lg text-som-green">♥️</span> and open source
+          &copy; Som Energia {new Date().getFullYear()} | Made with 💚 and open
+          source
         </p>
       </div>
     </footer>
