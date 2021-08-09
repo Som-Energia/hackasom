b86823f0d40b15ade7539cf4471a94b726e56e25
David García Garzón <david.garcia@somenergia.coop>
2021-08-09 10:13:14 +0200

    missing MarkDown component added

---
+++ src/components/MarkDown.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import ReactMarkdown from 'react-markdown'
+
+const MarkDown = (attributes) => {
+  const {className, children, ...remaining} = attributes
+  return (
+    <div
+      className={`markdown`}
+    >
+      <ReactMarkdown>
+      {children}
+      </ReactMarkdown>
+    </div>
+  )
+}
+
+export default MarkDown
